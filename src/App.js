2d51d1b5dcb65e2e93d92cbec3c0c0cd728d4dd6
ErikMoxley <exilerzero@yahoo.com>
2021-08-15 01:50:27 -0400

    moved styles folder to right one, src

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
+import "../src/styles/App.css";
 
 function App() {
   return (
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,3 +1,6 @@
+import React from "react";
+import "../styles/Content.css";
+
 const Content = () => {
     return (
       <div className="App">

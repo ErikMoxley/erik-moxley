93e0e73b2cd07f8976851dae8e8fdd9357ea19f1
ErikMoxley <exilerzero@yahoo.com>
2021-10-05 00:24:28 -0400

    update

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -2,8 +2,18 @@ const Projects = () => {
     return (
       <div className="content">
         <header className="title">
+        <h3>Projects</h3>
           <p>
-            Projects: Coming Soon!
+            NextJS Blog - <a href="https://moxleydevblog.netlify.app/" target="_blank" rel="noopener noreferrer">Live</a><span>&nbsp;</span><a href="https://github.com/ErikMoxley/moxleydevblog" target="_blank" rel="noopener noreferrer">Source</a>
+          </p>
+          <p>
+            Hashnode - <a href="https://hashnode.com/@MoxleyDev" target="_blank" rel="noopener noreferrer">Live</a>
+          </p>
+          <p>
+            Pokedex API <a href="https://github.com/ErikMoxley/pokedexapi" target="_blank" rel="noopener noreferrer">Source</a>
+          </p>
+          <p>
+            Movie Database Group Project - Coming Soon
           </p>
         </header>
       </div>

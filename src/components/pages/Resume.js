e7625b4d3e65c046c8361aa748007493a3be0d4a
ErikMoxley <exilerzero@yahoo.com>
2021-10-05 00:11:16 -0400

    final changes tor res section

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -4,18 +4,19 @@ import "../../styles/Content.css";
 const About = () => {
     return (
 <div class="timeline-container">
+<h3>Work History</h3>
   <div class="timeline">
     <ul>
       <li>
         <div class="timeline-content">
-          <h3 class="date">October 2021</h3>
+          <h4 class="date">October 2021</h4>
           <h1>Thermo Fisher Scientific</h1>
           <p>Manufacturer Technician I</p>
         </div>
       </li>
       <li>
         <div class="timeline-content">
-          <h3 class="date">November 2015 - May 2020</h3>
+          <h4 class="date">November 2015 - May 2020</h4>
           <h1>Walgreens Boots Alliance</h1>
           <p>Store Manager</p>
           <p>Assistant Store Manager</p>
@@ -23,7 +24,7 @@ const About = () => {
       </li>
       <li>
         <div class="timeline-content">
-          <h3 class="date">September 2007 - November 2015</h3>
+          <h4 class="date">September 2007 - November 2015</h4>
           <h1>Walgreens Co.</h1>
           <p>Shift Leader</p>
           <p>Customer Service Associate</p>
@@ -31,7 +32,7 @@ const About = () => {
       </li>
       <li>
         <div class="timeline-content">
-          <h3 class="date">November 2007 - October 2009</h3>
+          <h4 class="date">November 2007 - October 2009</h4>
           <h1>Genesis Healthcare</h1>
           <p>General Maintenance Technician</p>
         </div>

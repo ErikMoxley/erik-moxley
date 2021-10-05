7e05c355145f04f2889b2ce637d992a942ae37fb
ErikMoxley <exilerzero@yahoo.com>
2021-10-04 23:51:27 -0400

    reworked resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,91 +1,51 @@
 import ResumePdf from "../../assets/Eric-Kongkaew.pdf"
 import "../../styles/Content.css";
 
-import {
-  Timeline,
-  Container,
-  YearContent,
-  BodyContent,
-  Section,
-  Description,
- } from "vertical-timeline-component-react";
- 
- const Resume = () => {
-  const customTheme = {
-   yearColor: '#FFFFFF',
-   lineColor: '#d0cdc4',
-   dotColor: '#262626',
-   borderDotColor: '#d0cdc4',
-   titleColor: '#FFFFFF',
-   subtitleColor: '#bf9765',
-   textColor: '#000000',
-  };
- 
-  return (
-    <div className="content">
-      <h3>Work History</h3>
-      <div className="timeline">
-   <Timeline theme={customTheme} dateFormat='ll'>
-   <Container>
-     <YearContent startDate='2021/10' currentYear />
-     <BodyContent>
-      <Section title='Thermo Fisher Scientific'>
-        <Description variant='subtitle' text='Manufacturer Technician I' />
-        <Description text='Description' />
-      </Section>
-      </BodyContent>
-      </Container>
-
-    <Container>
-    <YearContent startDate='2015/11' endDate='2020/05' />
-    <BodyContent>
-     <Section title='Walgreens Boots Alliance'>
-      <Description variant='subtitle' text='Store Manager' />
-      <Description text='Description' />
-     </Section>
-
-     <Section>
-     <Description variant='subtitle' text='Assistant Manager' />
-      <Description text='Description' />
-     </Section>
-    </BodyContent>
-   </Container>
-
-   <Container>
-    <YearContent startDate='2007/09' endDate='2015/11' />
-    <BodyContent>
-     <Section title='Walgreens Co.'>
-      <Description variant='subtitle' text='Shift Leader' />
-      <Description text='Description' />
-     </Section>
-
-     <Section>
-     <Description variant='subtitle' text='Customer Service Associate' />
-      <Description text='Description' />
-     </Section>
-    </BodyContent>
-   </Container>
-
-   <Container>
-    <YearContent startDate='2007/11' endDate='2009/10' />
-    <BodyContent>
-     <Section title='Genesis Healthcare'>
-      <Description variant='subtitle' text='Maintenance Technician' />
-      <Description text='Description' />
-     </Section>
-    </BodyContent>
-   </Container>
-
-   </Timeline>
-   </div>
-  <p>
-      Download resume PDF:&nbsp;
+const About = () => {
+    return (
+<div class="timeline-container">
+  <div class="timeline">
+    <ul>
+      <li>
+        <div class="timeline-content">
+          <h3 class="date">October 2021</h3>
+          <h1>Thermo Fisher Scientific</h1>
+          <p>Manufacturer Technician I</p>
+        </div>
+      </li>
+      <li>
+        <div class="timeline-content">
+          <h3 class="date">November 2015 - May 2020</h3>
+          <h1>Walgreens Boots Alliance</h1>
+          <p>Store Manager</p>
+          <p>Assistant Store Manager</p>
+        </div>
+      </li>
+      <li>
+        <div class="timeline-content">
+          <h3 class="date">September 2007 - November 2015</h3>
+          <h1>Walgreens Co.</h1>
+          <p>Shift Leader</p>
+          <p>Customer Service Associate</p>
+        </div>
+      </li>
+      <li>
+        <div class="timeline-content">
+          <h3 class="date">November 2007 - October 2009</h3>
+          <h1>Genesis Healthcare</h1>
+          <p>General Maintenance Technician</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+    <p className='resume-link'>
+      Download Resume PDF:&nbsp;
     <span>
       <a href={ResumePdf} target="_blank" rel="noopener noreferrer">Here</a>
     </span>
-    </p>
+    </p>  
 </div>
-  );
- };
+    );
+  }
   
-  export default Resume;
+  export default About;

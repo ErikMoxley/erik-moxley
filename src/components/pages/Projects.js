8d365352daed7204df1b61e8c0ad51682979a3f4
ErikMoxley <exilerzero@yahoo.com>
2022-02-14 19:50:45 -0500

    made changes

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -4,9 +4,9 @@ const Projects = () => {
         <header className="title">
         <h3>Projects</h3>
           <p>
-            <b>Company Forms Page</b>
+            <b>Fullstack Company Forms Page</b>
             <br/>
-            Fullstack: HTML, CSS, JavaScript, NodeJS, Express, MySQL, GitHub Pages, Heroku (Mobile/Desktop Responsive)
+            HTML, CSS, JavaScript, NodeJS, Express, MySQL, GitHub Pages, Heroku (Mobile/Desktop Responsive)
             <br/>
             <a href="https://erikmoxley.github.io/fs-test-ek/" target="_blank" rel="noopener noreferrer">Live</a><span>&nbsp;</span><a href="https://github.com/ErikMoxley/fs-test-ek/" target="_blank" rel="noopener noreferrer">Source</a>
           </p>
